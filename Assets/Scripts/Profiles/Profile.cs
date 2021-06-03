783832873f3c0a8e0eb1a8c7fc2887f141945dcb
SergeyBenderskiy <hagal-the-greedy@yandex.ru>
2021-06-04 00:38:02 +0300

    Бэкап. Заведена структура для хранения профиля игрока.

---
+++ Assets/Scripts/Profiles/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Profiles
+{
+    [Serializable]
+    public sealed class Profile
+    {
+        [SerializeField] private List<Level> _levels;
+
+        [Serializable]
+        private struct Level
+        {
+            [SerializeField] private int _index;
+            [SerializeField] private int _reward;
+            [SerializeField] private float _time;
+        }
+    }
+}
