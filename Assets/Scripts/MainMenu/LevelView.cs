671b716ede9fa7ae255b0a7aec5fe99462e3cfcc
SergeyBenderskiy <hagal-the-greedy@yandex.ru>
2021-06-06 19:56:45 +0300

    Добавлено отображение рекордов скорости.

--- Assets/Scripts/MainMenu/LevelView.cs
+++ Assets/Scripts/MainMenu/LevelView.cs
@@ -10,12 +10,16 @@ namespace Menu
         public event Action<ushort> Selected;
 
         [SerializeField] private Text _levelNumber;
+        [SerializeField] private Text _bestTime;
         [SerializeField] private Button _button;
+
         private ushort _number;
 
         public void Setup(IGameLevel level)
         {
             _number = level.Index;
+            _bestTime.text = level.Stats != null ? level.Stats.Time.ToString("f2") : string.Empty;
+
             _levelNumber.text = ((ushort)(_number + 1)).ToString();
             _button.onClick.AddListener(OnSelected);
         }
